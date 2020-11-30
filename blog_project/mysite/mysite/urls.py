03029703ca3cf91fe6cb89f18b9a345a8dfadf82
SungYong <tjddyddl93@gmail.com>
2020-11-30 17:34:08 +0900

    views, html들 버그 해결중

--- blog_project/mysite/mysite/urls.py
+++ blog_project/mysite/mysite/urls.py
@@ -15,12 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.conf.urls import url, include
-from django.contrib.auth import login,logout
+from django.contrib.auth import views
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('blog.urls')),
-    url(r'account/login/$', login, name='login'),
-    url(r'account/logout/$', logout, name='logout', kwargs={'next_page': '/'})
+    url(r'account/login/$', views.LoginView.as_view(), name='login'),
+    url(r'account/logout/$', views.LogoutView.as_view(), name='logout', kwargs={'next_page': '/'})
 ]
